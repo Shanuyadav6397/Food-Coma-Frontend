ac7541f5f2f57b61d340dec4604ae78851ecef67
Shanu Yadav <yadavshanu023@gmail.com>
2025-02-07 18:02:35 +0530

    Add Home page and Arrow icons components

---
+++ src/Components/Icons/ArrowRight.jsx
@@ -0,0 +1,24 @@
+function ArrowRight(props) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            width="1em"
+            height="1em"
+            {...props}
+        >
+            <g
+                fill="none"
+                stroke="white"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="4"
+            >
+                <path d="M41.9999 24H5.99994"></path>
+                <path d="M30 12L42 24L30 36"></path>
+            </g>
+        </svg>
+    )
+}
+
+export default ArrowRight;
