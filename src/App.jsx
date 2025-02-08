03b14be4596460985fa55f728e9a36480ae0d768
Shanu Yadav <yadavshanu023@gmail.com>
2025-02-08 13:53:23 +0530

    Add NotFound component and implement 404 route handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import Signup from './Pages/Auth/Signup'
 import Login from './Pages/Auth/Login'
+import NotFound from './Pages/NotFound'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/auth/signup" element={<Signup />} />
         <Route path="/auth/login" element={<Login />} />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   )
